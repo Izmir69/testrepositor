573db2152dc3cc53e8aaa11f9f03482b814bf3cd
Izmir69 <mohamedizmir369@gmail.com>
2024-10-12 15:01:08 +0400

    ex

---
+++ pyhtonexercise.py
@@ -0,0 +1,22 @@
+print(5+3)
+print("python","is","fun!")
+print("She said, \"Hello!\"")
+print("mohamed\nizmir")
+print("Item 1\tItem 2\tItem 3")
+print("The result is", 42)
+print(12345)
+print(3.14159)
+print("Dollar sign: $")
+print("apple", "banana", "cherry", sep=", ")
+print("Hello,", end=" ")
+print("World!")
+print("Python! " * 3)
+print("\u2764") # Prints a heart symbol
+print()
+print("Hello " + "World!")
+print(True)
+print(False)
+print(1.2e3) # Prints 1200.0
+print("""This is a multiline
+print statement.""")
+print("This is a backslash: \\")
